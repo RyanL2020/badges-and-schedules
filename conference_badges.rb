8a9c87a6b7888e5d2bdf5bc281257dc1be6253c7
RL2020 <loc.fitness1@gmail.com>
2020-12-09 13:11:28 -0500

    commit tree 1ab939af47fe86bca962f34bb748bfb0dde4f58c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+
+
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(name)
+    name.map do |name|
+      "Hello, my name is #{name}."
+    end
+  end
+  
+  def assign_rooms(name)
+    name.map.with_index(1) do |name, num|
+      "Hello, #{name}! You'll be assigned to room #{num}!"
+    end
+  end
+  
+  def printer(name)
+    batch_badge_creator(name).each do |badge|
+      puts badge
+    end
+  
+    assign_rooms(name).each do |num|
+      puts num
+    end
+  end
